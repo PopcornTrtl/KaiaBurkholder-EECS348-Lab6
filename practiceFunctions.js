99b4d649edf926a125b7028f1c60a4ed68d774a3
Kaia Burkholder <143204235+PopcornTrtl@users.noreply.github.com>
2024-10-23 10:44:04 -0500

    Added files needed for practice1 and practice2

---
+++ practiceFunctions.js
@@ -0,0 +1,8 @@
+function changeBorder() {
+    let r = documents.forms["borderForm"]["borderR"].value;
+    let g = documents.forms["borderForm"]["borderG"].value;
+    let b = documents.forms["borderForm"]["borderB"].value;
+
+    documents.getElementById('paragraphEdit').style.color = "blue";
+
+}
