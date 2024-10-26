6f9fea28b02ef2d083bfe88f33ae0d27a18b0128
Kaia Burkholder <143204235+PopcornTrtl@users.noreply.github.com>
2024-10-26 12:54:46 -0500

    Delete practiceFunctions.js
    Deleted separate js file the scripts were put into the html due to issues

--- practiceFunctions.js
+++
@@ -1,8 +0,0 @@
-function changeBorder() {
-    let r = documents.forms["borderForm"]["borderR"].value;
-    let g = documents.forms["borderForm"]["borderG"].value;
-    let b = documents.forms["borderForm"]["borderB"].value;
-
-    documents.getElementById('paragraphEdit').style.color = "blue";
-
-}
